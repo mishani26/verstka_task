28ceffc8d35ce0f5b86c25aefdd87a3f9560fb60
Micheal <misha.badil@gmail.com>
2016-08-12 20:49:25 +0300

    sass now work

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,25 @@
-module.exports = function(grunt){
-	grunt.initConfig({
-		sass: {
-			options: {
-				sourceMap: true
-			},
-			dist: {
-				files: {
-					'style.css': 'style.scss'
-				}
-			}
-		},
-		watch:{
-			options:{
-				livereload:true
-			},
-			scripts:{
-				files:['верстка/*.scss'],
-				tasks:['newer:sass']
-			}
-		}
-	});
-	grunt.loadNpmTasks('grunt-sass');
-	grunt.loadNpmTasks("grunt-contrib-watch");
-	grunt.loadNpmTasks("grunt-newer");
-}
+module.exports = function(grunt) {
+    grunt.initConfig({
+        watch: {
+            sass: {
+                files: 'sass/style.scss',
+                tasks: ['sass']
+            }
+        },
+        sass: {                              // Task
+            dist: {                            // Target
+                options: {                       // Target options
+                    style: 'expanded',
+                    sourcemap: 'none'
+                },
+                files: {                         // Dictionary of files
+                    'css/new.css': 'sass/style.scss'       // 'destination': 'source
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.registerTask('default', ['sass']);
+    grunt.loadNpmTasks('grunt-contrib-watch')
+};
